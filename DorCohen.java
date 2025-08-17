e81902dc8a748287b491c91c8f40f5ad23d878e5
Dor11126 <cohen11126@gmail.com>
2025-08-17 22:40:20 +0300

    Second update - changed messages DorCohen.java

--- DorCohen.java
+++ DorCohen.java
@@ -1,7 +1,7 @@
 public class DorCohen {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Dor Cohen Updated! ..." + i);
+            System.out.println("Dor Cohen says Hi updated again! ..." + i);
         }
     }
 }
